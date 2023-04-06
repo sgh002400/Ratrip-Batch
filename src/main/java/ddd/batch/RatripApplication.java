c5c2465ac5461f3fe11206e6598fb0d1bbb6ecd1
jihwan <sgh002400@naver.com>
2023-04-06 22:13:02 +0900

    first commit

---
+++ src/main/java/ddd/batch/RatripApplication.java
@@ -0,0 +1,13 @@
+package ddd.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ddd/batch/RatripApplicationTests.java
@@ -0,0 +1,13 @@
+package ddd.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
